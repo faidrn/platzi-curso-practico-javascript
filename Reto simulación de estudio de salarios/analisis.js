8d95b50d5b630c7725f52f1902228825e5b8d560
Fredy Izquierdo <m3talmaniaco@gmail.com>
2022-06-29 00:47:23 -0500

    Update styles

--- Reto simulación de estudio de salarios/analisis.js
+++ Reto simulación de estudio de salarios/analisis.js
@@ -2,30 +2,30 @@
 [Video 22] Reto: simulación de estudio de salarios
 */
 
-const budgetArray = [];
 
 function calculateSavingCapacity(){
-    budgetArray.push({
-        InputIncome: Number(document.getElementById("InputIncome").value),
-        InputExpenses: Number(document.getElementById("InputExpenses").value)
-    });
+    const budgetArray = [
+        Number(document.getElementById("InputIncome").value),
+        Number(document.getElementById("InputExpenses").value)
+    ];
     // const InputIncome = Number(document.getElementById("InputIncome").value);
     // const InputExpenses = Number(document.getElementById("InputExpenses").value);
 
     const superavit = budgetArray.reduce(
-        function(sum = 0, income, expenses){
-            return sum = income - expenses;
+        function(income, expenses){
+            return income - expenses;
         }
     );
-    // let a = superavit[0];
-    // const superavit = budgetArray.sort(
-    //     function (income, expenses){
-    //         //si ingresos es mayor q gastos, devuelve un numero positivo
-    //         //si ingresos es igual a gastos, devuelve 0
-    //         //si ingresos es menor q gastos, devuelve un numero negativo
-    //         return income - expenses;
-    //     }
-    // );
 
-    console.log(superavit);
+    const supera = budgetArray.reduce((income, expenses) => { return income - expenses});
+
+    if (superavit > 0){
+
+    } else if(superavit === 0){
+
+    } else{
+
+    }
+    console.log(supera);
 }
+
