47a0a82537a0327da5ec1b75a5604bff64ab325c
faidrn <m3talmaniaco@gmail.com>
2022-06-30 17:57:14 -0500

    Add accordion js

--- Reto simulación de estudio de salarios/analisis.js
+++ Reto simulación de estudio de salarios/analisis.js
@@ -29,3 +29,27 @@ function calculateSavingCapacity(){
     console.log(supera);
 }
 
+
+/***ACOORDEON***/
+const bloque = document.querySelectorAll('.bloque');
+const h2 = document.querySelectorAll('.h2');
+//const contentTextAccordion = document.querySelectorAll('.content-text-accordion');
+
+//cuando haga Click en h2, 
+	// Quitar la clase activo de Todos los bloques
+	// añadir la clase activo al Bloque con la Posicion del h2
+
+//recorrer TODOS los h2
+h2.forEach((cadaH2, i) => {
+	//asignando un CLICK a cada h2
+	h2[i].addEventListener('click', () => {
+		//recorremos todos los bloques para quitar la clase activo
+		bloque.forEach((cadaBloque, j) => {
+			//quitar la clase activo de TODOS los bloques
+			bloque[j].classList.remove('activo')
+		})
+		//añadimos la clase activo al bloque cuya posicion sea igual al del h2 (linea 42)
+		bloque[i].classList.add('activo')
+	})
+});
+/***ACOORDEON***/
