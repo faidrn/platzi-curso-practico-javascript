69082d288910eb5cd85cd3bacb3f6002aa9c6cbd
faidrn <m3talmaniaco@gmail.com>
2022-07-01 11:17:01 -0500

    Add functions to get incomes and expenses arrays

--- Reto simulación de estudio de salarios/analisis.js
+++ Reto simulación de estudio de salarios/analisis.js
@@ -2,31 +2,111 @@
 [Video 22] Reto: simulación de estudio de salarios
 */
 
+let incomesArray = [];
+let incomesValuesArray = [];
+let expensesArray = [];
+let expensesValuesArray = [];
+let positionIncomes = 0;
+let positionExpenses = 0;
+
+function appendIncomes(){
+	const nameIncome = document.getElementById("selectIncomeType").value;
+	const incomeValue = Number(document.getElementById("InputIncome").value);
+	
+	if (nameIncome != ""){
+		if (incomeValue != ""){
+			
+			incomesArray.push({
+				income: nameIncome, 
+				incomeValue: incomeValue
+			});
+
+			incomesValuesArray[positionIncomes] = incomeValue;
+			positionIncomes += 1;
+
+			document.getElementById("selectIncomeType").value = "";
+			document.getElementById("InputIncome").value = "";
+
+			calculateSavingCapacity();
+			//console.log(incomesValuesArray);
+		} else{
+			return false;
+		}
+	} else{
+		return false;
+	}
+	
+}
+
+
+function appendExpenses(){
+	const nameExpense = document.getElementById("selectExpensesType").value;
+	const expenseValue = Number(document.getElementById("InputExpenses").value);
+	
+	if (nameExpense != ""){
+		if (expenseValue != ""){
+			expensesArray.push({
+				expense: nameExpense, 
+				expenseValue: expenseValue
+			});
+			
+			expensesValuesArray[positionExpenses] = expenseValue;
+			positionExpenses += 1;
+
+			document.getElementById("selectExpensesType").value = "";
+			document.getElementById("InputExpenses").value = "";
+
+			calculateSavingCapacity();
+		} else{
+			return false;
+		}
+	} else{
+		return false;
+	}
+	
+}
+
+function summation(valuesList){
+	const total = valuesList.reduce(
+		function(previousValue = 0, value){
+			return previousValue + value;
+		}
+	);
+	
+	return total;
+}
+
 
 function calculateSavingCapacity(){
-    const budgetArray = [
-        Number(document.getElementById("InputIncome").value),
-        Number(document.getElementById("InputExpenses").value)
-    ];
-    // const InputIncome = Number(document.getElementById("InputIncome").value);
-    // const InputExpenses = Number(document.getElementById("InputExpenses").value);
+	//Validar que los vectores de ingresos y egresos NO esten vacios
+	
+	if (incomesArray.length > 0){
+		if (expensesArray.length <= 0){
+			
+			//Sumar ingresos
+			const totalIncomes = summation(incomesValuesArray);
+			
+			//sumar egresos
+			const totalExpenses = summation(expensesValuesArray);
 
-    const superavit = budgetArray.reduce(
-        function(income, expenses){
-            return income - expenses;
-        }
-    );
+			const superavit = totalIncomes - totalExpenses;
 
-    const supera = budgetArray.reduce((income, expenses) => { return income - expenses});
+			/*const supera = budgetArray.reduce((income, expenses) => { return income - expenses});
 
-    if (superavit > 0){
+			if (superavit > 0){
 
-    } else if(superavit === 0){
+			} else if(superavit === 0){
 
-    } else{
+			} else{
 
-    }
-    console.log(supera);
+			}*/
+			console.log(totalIncomes);
+			
+		} else{
+			return false;
+		}
+	}
+    
 }
 
 
